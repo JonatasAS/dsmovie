c992ed0ceda2ed586def5e1316eb1e2b5a60878f
Jonatas_Amorim <jonatas231997@live.com>
2022-03-08 18:33:29 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/desmovie/DesmovieApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/desmovie/DesmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
