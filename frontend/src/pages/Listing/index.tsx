2962b47d4db57fb52230f9cb61aa7fc699f51793
Jonatas_Amorim <jonatas231997@live.com>
2022-03-10 00:48:19 -0300

    Navigation buttons

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -24,7 +24,7 @@ function Listing() {
                         <MovieCard />
                     </div>  
                     <div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
-                        <MovieCard />
+                        <MovieCard />                       
                     </div>                    
                 </div>
             </div>           
