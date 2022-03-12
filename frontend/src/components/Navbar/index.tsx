07c5c1d240200352107204937c380d841162862f
Jonatas_Amorim <jonatas231997@live.com>
2022-03-11 22:37:01 -0300

    Correção Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -8,7 +8,7 @@ function Navbar() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/JonatasAS"></a>
+                    <a href="https://github.com/JonatasAS">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
                             <p className="desmovie-contact-link">/JonatasAS</p>
